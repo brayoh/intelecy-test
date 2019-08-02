367fe3970ad95d6537ab0b8f3a42402693d21440
brayoh <bkuriakb@gmail.com>
2019-08-02 09:46:33 +0300

    feat(chart): show chart on data load

--- src/components/Graph/index.jsx
+++ src/components/Graph/index.jsx
@@ -13,38 +13,44 @@ import {
 /** Helper methods */
 import { formatMapWeatherData } from '../../utils/helpers';
 
-const Graph = ({ weatherData }) => (
-  <LineChart
-    width={1200}
-    height={600}
-    data={weatherData}
-    style={{ margin: 'auto' }}
-  >
-    <Line
-      type="monotone"
-      dataKey="temp"
-      stroke="#5b9d98"
-      name="temperature (degrees)"
-    />
-    <Line
-      type="monotone"
-      dataKey="tempMin"
-      stroke="#83e6b4"
-      name="min temperature (degrees)"
-    />
-    <Line
-      type="monotone"
-      dataKey="tempMax"
-      stroke="rgba(200, 0, 0, 0.8)"
-      name="max temperature (degrees)"
-    />
-    <CartesianGrid vertical={true} stroke="#e6f2ff" />
-    <XAxis tickLine={false} dataKey="name" />
-    <YAxis tickLine={false} />
-    <Tooltip />
-    <Legend />
-  </LineChart>
-);
+const Graph = ({ weatherData }) => {
+  return (
+    <React.Fragment>
+      {Object.keys(weatherData).length > 0 && (
+        <LineChart
+          width={1200}
+          height={600}
+          data={weatherData}
+          style={{ margin: 'auto' }}
+        >
+          <Line
+            type="monotone"
+            dataKey="temp"
+            stroke="#5b9d98"
+            name="temperature (degrees)"
+          />
+          <Line
+            type="monotone"
+            dataKey="tempMin"
+            stroke="#83e6b4"
+            name="min temperature (degrees)"
+          />
+          <Line
+            type="monotone"
+            dataKey="tempMax"
+            stroke="rgba(200, 0, 0, 0.8)"
+            name="max temperature (degrees)"
+          />
+          <CartesianGrid vertical={true} stroke="#e6f2ff" />
+          <XAxis tickLine={false} dataKey="name" />
+          <YAxis tickLine={false} />
+          <Tooltip />
+          <Legend />
+        </LineChart>
+      )}
+    </React.Fragment>
+  );
+};
 
 function mapStateToProps(state) {
   return {
